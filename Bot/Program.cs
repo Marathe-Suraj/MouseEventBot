c10fde34870ef26d89297c9c6b9a9aef569b478d
suraj <surajmarathe37@gmail.com>
2023-11-18 15:53:11 +0530

    Add project files.

---
+++ Bot/Program.cs
@@ -0,0 +1,14 @@
+﻿using System.Runtime.InteropServices;
+
+[DllImport("user32.dll")]
+static extern bool SetCursorPos(int X, int Y);
+
+Random r = new Random();
+
+while (true)
+{
+    int x = r.Next(300);
+    int y = r.Next(720);
+    SetCursorPos(x, y);
+    Thread.Sleep(2000);
+}
